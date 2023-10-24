67e78117459bd39593111977de6d7150d086e4fc
Sergey Kiprin <info@snkip.ru>
2023-10-24 09:42:51 +0700

    Edit templates

--- women/views.py
+++ women/views.py
@@ -4,14 +4,31 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 
 
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
+
+class MyClass():
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+
 def index(request):
     # t = render_to_string('women/index.html')
     # return HttpResponse(t)
-    return render(request, 'women/index.html')
+    data = {'title': 'Главная страница',
+            'menu': menu,
+            'float': 28.56,
+            'lst': [1, 2, 'abc', True],
+            'set': {1, 2, 3, 2, 5},
+            'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+            'obj': MyClass(10, 20),
+            }
+    return render(request, 'women/index.html', context=data)
 
 
 def about(request):
-    return render(request, 'women/about.html')
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
 
 def categories(request, cat_id):
